0be72810cd734221c0483c84588315d0e47d84cc
AB11898 <121186467+AB11898@users.noreply.github.com>
2022-12-21 22:06:27 +0500

    Create Sci_Calculator.py

---
+++ Sci_Calculator.py
@@ -0,0 +1,90 @@
+import math
+
+
+def add(x, y):
+    return x + y
+
+
+def subtract(x, y):
+    return x - y
+
+
+def multiply(x, y):
+    return x * y
+
+
+def divide(x, y):
+    return x / y
+
+
+def sin(x):
+    return math.sin(x)
+
+
+def tan(x):
+    return math.tan(x)
+
+
+def cos(x):
+    return math.cos(x)
+
+
+def acos(x):
+    return math.acos(x)
+
+
+def asin(x):
+    return math.asin(x)
+
+
+def atan(x):
+    return math.atan(x)
+
+
+def log(x):
+    return math.log(x)
+
+
+def sqrt(x):
+    return math.sqrt(x)
+
+
+def average(numbers):
+    return sum(numbers) / len(numbers)
+
+
+def percentage(part, whole):
+    return (part / whole) * 100
+
+
+def power(x, y):
+    return x ** y
+
+
+def cube(x):
+    return x ** 3
+
+
+#  Some function called
+
+result = add(5, 10)
+print(result)  # prints 15
+
+result = subtract(5, 10)
+print(result)  # prints 5
+
+result = multiply(5, 10)
+print(result)  # prints 50
+
+result = divide(10, 5)
+print(result)  # prints 2
+
+result = sin(math.pi / 2)
+print(result)  # prints 1.0
+
+result = average([1, 2, 3])
+print(result)  # prints 2.0
+
+result = percentage(5, 100)
+print(result)  # prints 5.0
+
